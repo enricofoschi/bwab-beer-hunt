4ac3cca2e8868c21789f8b98ef3bfc5bfc442a33
Enrico Foschi <foschi.enrico@gmail.com>
2015-11-23 18:44:10 +0100

    Wiring up votes on the UI and adding a vote up event

--- client/beers.js
+++ client/beers.js
@@ -3,3 +3,13 @@ Template.beers.helpers({
         return beers.find().fetch();
     }
 });
+
+Template.beers.events({
+    'click .action-vote': function() {
+        beers.update(this._id, {
+            $set: {
+                vote: (this.vote || 0) + 1
+            }
+        });
+    }
+});
